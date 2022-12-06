ae69ab9b93f62f69388fef89130cc2346b168852
RuthJimenez309 <79241546+RuthJimenez309@users.noreply.github.com>
2022-12-06 05:57:26 -0600

    ModificadaPago

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-const bd:string = 'usuario';
-const port:string = '27017';
-//por medio de la ip se hizo el llamado y se conecto a mongo  cambiar esto por que solo fue una prueba 
-const host:string = '127.0.0.1';
 
 export class Database {
     homer: string = 'Homero_Delivery';
     host:string = 'localhost';
     port: string = '27017';
+    URI:string= `mongodb://${this.host}:${this.port}/${this.homer}`;
 
     constructor (){
         this.conectar();
